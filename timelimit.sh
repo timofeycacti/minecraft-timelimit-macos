81682769dfa6a524e5fdd20c6ac8613d2709a580
Timoshka <timoshka@Antons-MBP-2.fritz.box>
2023-10-31 20:17:17 +0100

    small changes

--- timelimit.sh
+++ timelimit.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 time=0
+killed="false"
 
 while :
 do
-	if [ $(cat ./timeadd.txt) != "0" ]; then
-		time=$(($time-$(cat ./timeadd.txt)))
-		echo "0" > ./timeadd.txt
-	fi
+  if [ $(cat ./timeadd.txt) != "0" ]; then
+    time=$(($time-$(cat ./timeadd.txt)))
+    echo "0" > ./timeadd.txt
+  fi
 
-	ps -e | grep -v "grep" |  grep -q '/minecraft/runtime/java-runtime-gamma/'
-	if [ $? -eq 0 ]; then
-		((time++))
-     	echo "time=${time}"
-    else
-		echo "MINECRAFT is not running."
-	fi
+  ps -e | grep -v "grep" |  grep -q '/minecraft/runtime/java-runtime-gamma/'
+  if [ $? -eq 0 ]; then
+    ((time++))
+    echo "time=${time}"
+  else
+    echo "MINECRAFT is not running."
+  fi
 
-	if [ $time -eq 25 ]; then
-		say ten minutes left
-	fi
+  if [ $time -eq 25 ]; then
+    say "ten minutes left"
+  fi
  
-	if [ $time -gt 34 ]; then
-        kill -9 $(ps -e | grep -v "grep" | grep '/minecraft/runtime/java-runtime-gamma/' | cut -f1 -d" ")
-    fi
+  if [ $time -gt 34 ] && [ $killed == "false" ]; then
+    say "I am going to kill minecraft!"
+    kill -9 $(ps -e | grep -v "grep" | grep '/minecraft/runtime/java-runtime-gamma/' | cut -f1 -d" ")
+    killed="true"
+  fi
 
-	sleep 1
+  sleep 1
 done
