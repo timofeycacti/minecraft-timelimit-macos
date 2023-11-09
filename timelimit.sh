d34b69697e28fdd35710e5ec02a09b71143a2d35
Timoshka <timoshka@Antons-MBP-2.fritz.box>
2023-11-09 22:38:32 +0100

    added logging

--- timelimit.sh
+++ timelimit.sh
@@ -13,22 +13,24 @@ do
   ps -e | grep -v "grep" | grep -q '/minecraft/runtime/java-runtime-gamma/' && IS_MINECRAFT_RUNNING=0 || IS_MINECRAFT_RUNNING=1
   if [ $IS_MINECRAFT_RUNNING -eq 0 ]; then
     ((time++))
-    echo "time=${time}"
+    echo "$(date) time=${time}" | tee -a ${script_dir}/log.log
   else
-    echo "MINECRAFT is not running."
+    echo "$(date) MINECRAFT is not running." | tee -a ${script_dir}/log.log
   fi
 
   if [ $time -eq 25 ]; then
     say "ten minutes left"
+    echo "$(date) ten minutes left" | tee -a ${script_dir}/log.log
   fi
  
   if [ $time -gt 34 ] && [ $IS_MINECRAFT_RUNNING -eq 0 ]; then
     say "I am going to kill minecraft!"
+    echo "$(date) I am going to kill minecraft!" | tee -a ${script_dir}/log.log
     kill -9 $(ps -e | grep -v "grep" | grep '/minecraft/runtime/java-runtime-gamma/' | xargs | cut -f1 -d" ")
   fi
 
   if [ $(date | cut -f5 -d" " | cut -f1,2 -d":") = "00:00" ]; then
-    echo "TIME RESET"
+    echo "$(date) TIME RESET" | tee -a ${script_dir}/log.log
     time=0
   fi
   
